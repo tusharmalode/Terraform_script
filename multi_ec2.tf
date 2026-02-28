bae9bf0f12207e5f40afd596abd7706ed3e266d9
tusharmalode <tusharmalode4670@gmail.com>
2026-02-28 15:52:09 +0530

    Create multiple instance at time using key count

---
+++ multi_ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "count" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+  key_name      = "ansi-key"
+  tags = {
+    Name = "demo ${count.index+1}"
+  }
+  count = 2
+
+}
