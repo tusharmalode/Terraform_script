0c12e1dee586383d1e7f611c9ad60ba24bfd39e7
tusharmalode <tusharmalode4670@gmail.com>
2026-02-28 16:08:39 +0530

    we can provide the name to each instance using for_each key toset for each

---
+++ multi_ec2_with_differnt_name.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "demo" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+  key_name      = "ansi-key"
+  tags = {
+    Name = each.key
+  }
+  for_each = toset(["web", "app", "db"])
+
+}
